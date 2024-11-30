a33bcc00130ba45f4235ef9a739e48184b5d4582
sumit jaiswal <sumitjaiswal263@gmail.com>
2024-12-01 03:27:04 +0530

    scrapeupdate

--- utils/problemScraper.js
+++ utils/problemScraper.js
@@ -1,9 +1,13 @@
-const puppeteer = require("puppeteer");
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer-core");
 
 async function scrapeProblemWithBrowser(contestId, problemId) {
   const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
   });
 
   try {
